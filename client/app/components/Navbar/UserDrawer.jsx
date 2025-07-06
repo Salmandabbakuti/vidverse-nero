73b2b655c7c617da916dcc357a3965a31b6b3a60
Salman Dabbakuti <29351207+Salmandabbakuti@users.noreply.github.com>
2025-07-06 22:59:04 +0530

    fix: drawer being kept open on menu item click

--- client/app/components/Navbar/UserDrawer.jsx
+++ client/app/components/Navbar/UserDrawer.jsx
@@ -72,6 +72,7 @@ export default function UserDrawer() {
         }
       >
         <Menu
+          onClick={() => setDrawerOpen(false)}
           items={[
             ...(aaWalletAddress
               ? [
